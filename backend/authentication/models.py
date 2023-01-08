c1c3fec7edc38aa64315cdc071f84dd674c7c755
Satyam Sagar <satyamsagar1711@gmail.com>
2023-01-08 07:07:10 +0530

    removed photos

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     address = models.TextField()
     phone = models.CharField(max_length=20)
     college = models.CharField(max_length=100)
-    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    # profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
     batch = models.CharField(max_length=20)
 
 
