525b82fa7e0367b29dbb506a925a9bd6d0d69f4f
Satyam Sagar <satyamsagar1711@gmail.com>
2023-01-17 04:18:29 +0530

    minor

--- backend/backpack/settings.py
+++ backend/backpack/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
