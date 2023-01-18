04cf9f8139e75560d993adeb100a52b327a56d52
Tarun Garg <96339593+Evil2004@users.noreply.github.com>
2023-01-18 20:01:02 +0530

    fix: urls for products

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ProductViewSet, RequestProduct, AcceptOrRejectRequest, Profile
 
 urlpatterns = [
-    path('products', ProductViewSet.as_view(), name='products'),
-    path('request-product', RequestProduct.as_view(), name='request-product'),
-    path('accept-or-reject-request', AcceptOrRejectRequest.as_view(), name='accept-or-reject-request'),
-    path('profile',Profile.as_view(),name='profile'),
+    path('products/', ProductViewSet.as_view(), name='products'),
+    path('request-product/', RequestProduct.as_view(), name='request-product'),
+    path('accept-or-reject-request/', AcceptOrRejectRequest.as_view(), name='accept-or-reject-request'),
+    path('profile/',Profile.as_view(),name='profile'),
 ]
