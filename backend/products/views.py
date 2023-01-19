36fc158b253f07ab2d125fd84ca1c06918e96320
Satyam Sagar <satyamsagar1711@gmail.com>
2023-01-19 19:12:25 +0530

    fix: transaction dictionary issue

--- backend/products/views.py
+++ backend/products/views.py
@@ -67,14 +67,6 @@ class RequestProduct(generics.GenericAPIView, CreateModelMixin):
     serializer_class = TransactionSerializer
     permission_classes = [IsSameCollegeButNotSelf]
 
-    def create(self, request, *args, **kwargs):
-        print(request.data, 'views')
-        serializer = self.get_serializer(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
     def post(self, request, *args, **kwargs):
         subject = f'New Request for your product {Product.objects.get(id=request.data["product"]).name}'
         message = f'Hi {Product.objects.get(id=request.data["product"]).current_owner.username}, {request.user.username} has requested for your product {Product.objects.get(id=request.data["product"]).name}. \nPlease login to your account to accept or reject the request.\n Or directly click on the link below to accept the request.\n http://todo.com'
