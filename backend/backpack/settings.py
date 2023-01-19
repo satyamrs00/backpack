bd31062be93cd28eaf4ce5b7b18028cde29773e8
Tarun Garg <96339593+Evil2004@users.noreply.github.com>
2023-01-19 15:22:55 +0530

    cors origin bug fix

--- backend/backpack/settings.py
+++ backend/backpack/settings.py
@@ -149,7 +149,9 @@ CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
-CORS_ALLOWED_ORIGINS = ALLOWED_HOSTS
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_ALL_ORIGINS = False
