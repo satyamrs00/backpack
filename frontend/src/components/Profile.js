9195c980006ddfcd46e8c6f8f608cc739ede4ffa
DhruvRajpoot <dhruv.kutarya@gmail.com>
2023-01-19 14:32:21 +0530

    product,profile api bug fixes

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -3,15 +3,14 @@ import Modal from 'react-bootstrap/Modal';
 import ThemeContext from "../context/ThemeContext";
 import AuthContext from "../context/AuthContext";
 import ModalHeader from "react-bootstrap/esm/ModalHeader";
+import ProductContext from "../context/ProductContext";
 const Profile = () => {
-  const { checkUser, profile } = useContext(AuthContext)
+  const { checkUser } = useContext(AuthContext)
   useEffect(()=>{checkUser()},[])
-  
-  const [data,setData]=useState()
+  const {profileData,profile}=useContext(ProductContext)
   useEffect(()=>{
-    setData(profile())
-  },[])
-
+    profile()
+},[])
   const { theme, inputStyle } = useContext(ThemeContext)
   const [myStyle, setMyStyle] = useState({})
   const [inputMyStyle, setInputMyStyle] = useState({})
@@ -38,8 +37,8 @@ const Profile = () => {
       <Modal show={show} onHide={handleClose} style={{ backdropFilter: 'blur(2px)', overflow: 'visible' }}>
         <ModalHeader closeButton style={{ ...inputStyle }}></ModalHeader>
         <Modal.Body style={{ padding: '1.5rem', ...inputStyle }}>
-          {data.request_to_me.length === 0 && <p className="fs-5 fst-italic">No request now</p>}
-          {data.request_to_me.length > 0 &&
+          {(profileData.request_to_me?profileData.request_to_me.length:0) === 0 && <p className="fs-5 fst-italic">No request now</p>}
+          {(profileData.request_to_me?profileData.request_to_me.length:0) > 0 &&
             <div className="d-flex justify-content-end">
               <button className='btn btn-success shadow-sm me-2' onClick={handleClose}>Accept</button>
               <button className='btn btn-danger shadow-sm' onClick={handleClose}>Decline</button>
@@ -52,29 +51,29 @@ const Profile = () => {
         <div className="row w-100">
           <div className={`col-md-5 ${window.screen.width > 992 ? 'd-flex justify-content-center' : ''}`}>
             <div style={{ minWidth: "calc(10rem + 10vw)" }}>
-              <img src={data.user.profile_pic} alt="" width={110} height={100} />
+              <img src={profileData.user?profileData.user.profile_pic:''} alt="" width={110} height={100} />
               <h4 className="mt-4">
-                {data.user.first_name}  {data.user.last_name}
+                {profileData.user?profileData.user.first_name:''}  {profileData.user?profileData.user.last_name:''}
               </h4>
-              <h6 className="mt-3">{data.user.username}</h6>
-              <h6 className="mt-3 mb-4">{data.user.email}</h6>
+              <h6 className="mt-3">{profileData.user?profileData.user.username:''}</h6>
+              <h6 className="mt-3 mb-4">{profileData.user?profileData.user.email:''}</h6>
               <hr />
               <h4 className="mt-4">About</h4>
               <ul className="list p-0">
                 <li style={{ ...inputMyStyle }} className="list-group-item fw-bold mt-3">College</li>
-                <li style={{ ...inputMyStyle }} className="list-group-item mt-1">{data.user.college}</li>
+                <li style={{ ...inputMyStyle }} className="list-group-item mt-1">{profileData.user?profileData.user.college:''}</li>
                 <li style={{ ...inputMyStyle }} className="list-group-item fw-bold mt-3">Batch</li>
-                <li style={{ ...inputMyStyle }} className="list-group-item mt-1">{data.user.batch}</li>
+                <li style={{ ...inputMyStyle }} className="list-group-item mt-1">{profileData.user?profileData.user.batch:''}</li>
                 <li style={{ ...inputMyStyle }} className="list-group-item  fw-bold mt-3">Address</li>
-                <li style={{ ...inputMyStyle }} className="list-group-item mt-1">{data.user.address}</li>
+                <li style={{ ...inputMyStyle }} className="list-group-item mt-1">{profileData.user?profileData.user.address:''}</li>
                 <li style={{ ...inputMyStyle }} className="list-group-item fw-bold mt-3">Mobile</li>
-                <li style={{ ...inputMyStyle }} className="list-group-item mt-1">{data.user.phone}</li>
+                <li style={{ ...inputMyStyle }} className="list-group-item mt-1">{profileData.user?profileData.user.phone:''}</li>
               </ul>
             </div>
           </div>
           <div className="col-md-7">
             <div className="d-flex justify-content-end">
-              <button className="btn position-relative shadow-none" onClick={handleShow} style={{ backgroundColor: "orange", fontWeight: '500', color: '#404040' }} >New Requests {data.request_to_me.length > 0 && <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+              <button className="btn position-relative shadow-none" onClick={handleShow} style={{ backgroundColor: "orange", fontWeight: '500', color: '#404040' }} >New Requests {(profileData.request_to_me?profileData.request_to_me.length:0) > 0 && <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
               </span>}</button>
             </div>
           </div>
@@ -95,10 +94,10 @@ const Profile = () => {
           <div className="col-md-3" style={{ paddingLeft: "10rem" }}>
             <img src="https://picsum.photos/100/100" alt="" srcset="" />
             <h4 className="mt-4">
-              {data.user.first_name}  {data.user.last_name}
+              {profileData.user?profileData.user.first_name}  {profileData.user?profileData.user.last_name}
             </h4>
-            <h5 className="mt-3">{data.user.username}</h5>
-            <h5 className="mt-3">{data.user.email}</h5>
+            <h5 className="mt-3">{profileData.user?profileData.user.username}</h5>
+            <h5 className="mt-3">{profileData.user?profileData.user.email}</h5>
             <hr />
           </div>
           <div className="col-md-9 " style={{ paddingLeft: "10rem" }}>
@@ -147,13 +146,13 @@ const Profile = () => {
             <h3 className="mt-5">About</h3>
             <ul className="list " style={{ paddingLeft: "0" }}>
               <li style={{...inputMyStyle}} className="list-group-item fw-bold mt-3">College</li>
-              <li style={{...inputMyStyle}} className="list-group-item mt-1">{data.user.college}</li>
+              <li style={{...inputMyStyle}} className="list-group-item mt-1">{profileData.user?profileData.user.college}</li>
               <li style={{...inputMyStyle}} className="list-group-item fw-bold mt-3">Batch</li>
-              <li style={{...inputMyStyle}} className="list-group-item mt-1">{data.user.batch}</li>
+              <li style={{...inputMyStyle}} className="list-group-item mt-1">{profileData.user?profileData.user.batch}</li>
               <li style={{...inputMyStyle}} className="list-group-item  fw-bold mt-3">Address</li>
-              <li style={{...inputMyStyle}} className="list-group-item mt-1">{data.user.address}</li>
+              <li style={{...inputMyStyle}} className="list-group-item mt-1">{profileData.user?profileData.user.address}</li>
               <li style={{...inputMyStyle}} className="list-group-item fw-bold mt-3">Mobile</li>
-              <li style={{...inputMyStyle}} className="list-group-item mt-1">{data.user.phone}</li>
+              <li style={{...inputMyStyle}} className="list-group-item mt-1">{profileData.user?profileData.user.phone}</li>
             </ul>
           </div>
           <div className="col-md-9 mt-4" style={{ paddingLeft: "10rem" }}>
