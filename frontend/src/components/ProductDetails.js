f66ace41b6fbfc7b1d4cbf1de30c5dfdb2a3679f
DhruvRajpoot <dhruv.kutarya@gmail.com>
2023-01-21 06:34:18 +0530

    product details modified

--- frontend/src/components/ProductDetails.js
+++ frontend/src/components/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect,useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import Carousel from './Carousel'
 import available from './images/available.png'
@@ -11,11 +11,11 @@ import { baseurl } from '../baseurl'
 
 export default function ProductDetails() {
     const api = useAxios()
-    const { myStyle } = useContext(ThemeContext)
+    const {theme,myStyle} = useContext(ThemeContext)
     const { checkUser, loading } = useContext(AuthContext)
     // eslint-disable-next-line
     useEffect(() => { checkUser() }, [])
-    const capitalize=(str)=>{
+    const capitalize = (str) => {
         return (str[0].toUpperCase() + str.slice(1))
     }
 
@@ -36,31 +36,59 @@ export default function ProductDetails() {
         await api.post(url, formData)
     }
 
+    const [myStyle2, setMyStyle2] = useState({
+        background: 'rgb(240 240 240)',
+        color: '#181818',
+    })
+    useEffect(() => {
+        if (theme === 'dark') {
+            setMyStyle2({
+                background: '#252525',
+                color: 'white'
+            })
+        }
+        else {
+            setMyStyle2({
+                background: 'rgb(240 240 240)',
+                color: 'black'
+            })
+        }
+    }, [theme])
+
     return (
         <>
             {loading && <Loading />}
             {!loading &&
                 <div style={{ ...myStyle, boxShadow: '0 0 10px grey', padding: '1.5rem', border: "4px outset lightgrey" }} className='container rounded-3'>
                     <div className="row">
-                        <div className={`col-${window.screen.width > 900 ? 6 : 12} d-flex justify-content-center rounded`}>
+                        <div className={`col-${window.screen.width > 900 ? 6 : 12} d-flex justify-content-center rounded`} style={{marginBottom:"calc(1.5rem - 1.5vw)"}}>
                             <Carousel images={imageArr} width='calc(10rem + 25vw)' height='calc(12rem + 23vw)' />
                         </div>
-                        <div style={{ fontFamily: 'serif' }} className={`col-${window.screen.width > 900 ? 6 : 12}`}>
-                            <div className='row' style={{ padding: '1rem 1.5rem .1rem 1.5rem' }}>
+                        <div style={{ fontFamily: 'Roboto Slab', position: "relative",...myStyle2 }} className={`col-${window.screen.width > 900 ? 6 : 12}`}>
+                            <div className='row' style={{ padding: '1rem 1.5rem .1rem 1.5rem'}}>
                                 <h5 className='col' style={{ fontSize: 'calc(1.7rem + .3vw)' }}>{capitalize(data.name)}</h5>
                                 <img className='col-2' src={data.available ? available : notavailable} alt={data.available ? 'available' : 'not available'} style={{ width: 'calc(3.5rem + .5vw)', height: "calc(2rem + .5vw)" }} />
                             </div>
-                            <div style={{ padding: '1rem 1.5rem .5rem 1.5rem' }}>
-                                <h6 style={{ fontSize: 'calc(1rem + .3vw)', fontWeight: '600',letterSpacing:'.5px' }}>Description</h6>
-                                <p style={{ fontSize: 'calc(.9rem + .3vw)' }}>{data.description}</p>
+                            <div style={{ padding: '1.5rem 1.5rem .5rem 1.5rem' }}>
+                                <h6 style={{ fontSize: 'calc(1rem + .1vw)', letterSpacing: '.5px' }}>About this book</h6>
+                                <p style={{ fontSize: 'calc(.9rem + .2vw)',fontWeight:"600",wordBreak:'break-all' }}>{data.description}</p>
                             </div>
                             <div style={{ padding: '0rem 1.5rem .5rem 1.5rem' }}>
-                                <h6 style={{ fontSize: 'calc(1rem + .3vw)' }}>Owner : <span style={{ fontWeight: '900' }}>{data.owner.first_name + " " + data.owner.last_name}</span></h6>
-                                <h6 style={{ fontSize: 'calc(1rem + .3vw)' }}>Current Owner : <span style={{ fontWeight: '900' }}>{data.current_owner.first_name + " " + data.current_owner.last_name}</span></h6>
+                                <h6 style={{ fontSize: 'calc(1rem + .1vw)', letterSpacing: '.5px' }}>Owner Details</h6>
+                                Name : <span style={{ fontSize: 'calc(1rem + .1vw)', fontWeight: '600' }}>{capitalize(data.owner.first_name) + " " + capitalize(data.owner.last_name)}</span>
+                                <br />
+                                Batch : <span style={{ fontSize: 'calc(1rem + .1vw)', fontWeight: '600' }}>{data.owner.batch}</span>
+                                <br /><br />
+                                <h6 style={{ fontSize: 'calc(1rem + .1vw)', letterSpacing: '.5px' }}>Current Owner Details</h6>
+                                Name : <span style={{ fontSize: 'calc(1rem + .1vw)', fontWeight: '600' }}>{capitalize(data.current_owner.first_name) + " " + capitalize(data.current_owner.last_name)}</span>
+                                <br /> 
+                                Batch : <span style={{ fontSize: 'calc(1rem + .1vw)', fontWeight: '600' }}>{data.current_owner.batch}</span>
                             </div>
-                            <div className="footer d-flex justify-content-end" style={{ backgroundColor: 'rgb(187 187 187)', padding: '.5rem 1rem' }}>
-                                {data.available && <button className='btn shadow-sm' style={{ backgroundColor: "orange", fontWeight: '600', color: '#404040' }} onClick={handleClick}>Request Book</button>}
-                                {!data.available && <span className='text-danger p-1'>Currently unavailable</span>}
+                            <div className={`${window.screen.width>992?'position-absolute':''} bottom-0`} style={{ width: '97%' }}>
+                                <div className="footer d-flex justify-content-end" style={{ backgroundColor: 'rgb(187 187 187)', padding: '.5rem 1rem',marginBottom:".5rem" }}>
+                                    {data.available && <button className='btn shadow-sm' style={{ backgroundColor: "orange", fontWeight: '600', color: '#404040' }} onClick={handleClick}>Request Book</button>}
+                                    {!data.available && <span className='text-danger p-1'>Currently unavailable</span>}
+                                </div>
                             </div>
                         </div>
                     </div>
