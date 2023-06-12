0b69afa1df1920404c30ccecdd04f26e122ab9f0
Satyam Sagar <satyamsagar1711@gmail.com>
2023-06-13 05:02:32 +0530

    adapted to some changes in backend

--- frontend/src/baseurl.js
+++ frontend/src/baseurl.js
@@ -1,2 +1,3 @@
 export const baseurl='https://backpack-backend.onrender.com/'
 // export const baseurl='https://11f5-2405-201-3002-e132-319a-f765-6b25-fbc3.in.ngrok.io/'
+// export const baseurl = 'http://localhost:8000/'
